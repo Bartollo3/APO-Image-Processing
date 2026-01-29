014eb2542c46626008a9fb14df3456e0b606de28
Bartollo3 <38593184+Bartollo3@users.noreply.github.com>
2026-01-29 16:11:18 +0100

    App base structure

---
+++ Logical.py
@@ -0,0 +1,12 @@
+class Logical:
+    def logical_not(self):
+        pass
+
+    def logical_operations(self, operation):
+        match operation:
+            case "and":
+                pass
+            case "or":
+                pass
+            case "xor":
+                pass
---
+++ Morphological.py
@@ -0,0 +1,17 @@
+class Morphological:
+    def morphology_dialog(self, operation):
+        match operation:
+            case 'erode':
+                pass
+            case 'dilate':
+                pass
+            case 'open':
+                pass
+            case 'close':
+                pass
+
+    def reconstruction_dialog(self):
+        pass
+
+    def skeletonize_dialog(self):
+        pass
